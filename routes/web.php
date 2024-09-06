1bf880dbec6c74d80bdde54c830b74996ff814b3
yadzka17 <yadzka17@gmail.com>
2024-09-06 23:03:13 +0700

    Memotong File Layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('layouts/app');
-});
+// Route::get('/', function () {
+//     return view('dashboard.home');
+
+// });
+
+// Route::get('/login', function () {
+//     return view('auth.login');
+
+// })->name('login');
+
+// Route::get('/register', function () {
+//     return view('auth.register');
+
+// })->name('register');
+
+// Route::get('/reset', function () {
+//     return view('auth.reset');
+
+// })->name('reset');
+
+// Route::get('/forgot', function () {
+//     return view('auth.forgot');
+
+// })->name('forgot');
